5c3a553ec3192659e1432577b94346bbd86d6f76
marwa.mejri <marwa.mejri@proxym-it.com>
2022-09-14 10:49:39 +0100

    add base useCases

---
+++ lib/core/commundomain/usecases/base_usecase.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_architecture/core/commundomain/entitties/based_api_result/api_result_model.dart';
+
+abstract class BaseUseCase<Type> {
+  Future<ApiResultModel<Type>> invoke();
+}
